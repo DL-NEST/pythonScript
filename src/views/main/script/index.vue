d4acb73ecf824b1ba6b5c15463927b4072d6ed89
DL-NEST <76174583+DL-NEST@users.noreply.github.com>
2021-10-27 17:35:06 +0800

    main

--- src/views/main/script/index.vue
+++ src/views/main/script/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="script"></div>
+  <div class="script">
+    <div class="script-item" v-for="item in 20" :key="item"></div>
+  </div>
 </template>
 
 <script>
@@ -15,9 +17,26 @@ export default {
 
 <style lang="scss" scoped>
 .script{
-  position: relative;
+  -webkit-app-region: no-drag;
+  position: absolute;
   width: 100%;
-  height: 100%;
+  height: 86%;
   background-color: #9efcc2;
+  overflow-y: auto;
+  overflow-x: hidden;
+  border-radius: 0 0 4px 4px;
+  // 布局
+  box-sizing:border-box;
+  padding: 10px;
+
+  .script-item{
+    width: 100%;
+    height: 60px;
+    margin-bottom: 5px;
+    background-color: #2c3e50;
+  }
+}
+.script::-webkit-scrollbar {
+  width: 0;
 }
 </style>
